a6a6f47b35696b9403a39f8b607625dd4ca4ed62
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2018-09-30 14:18:18 +0300

    Update Pr20.py

--- Pr20.py
+++ Pr20.py
@@ -4,8 +4,5 @@ def factorial(n):
 	else:
 		return n*factorial(n-1)
 	
-
-num = factorial(100)
-f = [int(x) for x in str(num)]	
-ans = sum([f[x] for x in range(0,len(f))])
-print(ans)
+f = [int(x) for x in str(factorial(100))]	
+print(sum(f[x] for x in range(0,len(f))))
