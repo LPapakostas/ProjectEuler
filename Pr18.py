d2156038d929b64d9c4c7ecb94e2eb8cdf17bdc9
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2018-10-09 21:28:25 +0300

    Add files via upload

---
+++ Pr18.py
@@ -0,0 +1,10 @@
+def triag(data):
+	for i in range(len(data)-2,-1,-1):
+		for j in range(i+1):
+			data[i][j]+=max(data[i+1][j],data[i+1][j+1])
+	return data[0][0]
+
+f = open("Pr18_numbers.txt","r")
+data = [[int(x) for x in line.split()] for line in f]
+print(triag(data))
+
