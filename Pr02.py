c064f32b1c62252f123323f23a7803b10394d770
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2018-11-01 22:25:48 +0200

    Update Pr02.py

--- Pr02.py
+++ Pr02.py
@@ -1,13 +1,11 @@
-import math
-import time
+import math,time
 start_time = time.time()
 
 (f0,f1) = (1,1)
-ans =0
-while (f1 < 4000000):
+ans = 0 ; N = 4000000
+while (f1 < N):
     if ( f1%2 == 0):
         ans+=f1
-    (f0,f1) = (f1,f1+f0)
-    
+    (f0,f1) = (f1,f1+f0)    
 print(ans)
 print(time.time() - start_time,"seconds")
