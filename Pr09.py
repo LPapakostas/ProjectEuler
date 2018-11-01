d55f0b8f04fa049c94b56dfb0efa4cb43e967f5f
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2018-11-01 23:11:04 +0200

    Update Pr09.py

--- Pr09.py
+++ Pr09.py
@@ -1,6 +1,13 @@
-for c in range(1,1000):
-   for b in range(1,c):
-       for a in range(1,b):
-            if( a+b+c == 1000 and c**2 == a**2 + b**2):
-                print(a*b*c)
+import time
+def pythagorian_tri(N):
+    for c in range(1,N):
+       for b in range(1,c):
+           for a in range(1,b):
+                if (a+b+c == N) and (c*c == a*a+b*b):
+                    return a*b*c
+    return 0
+
+start = time.time() ; N = 1000 
+print(pythagorian_tri(N),time.time()-start,"seconds")
+
     
