cba13bbcb2ad303eebdc7fe551569324b511733b
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2018-11-01 22:43:57 +0200

    Update Pr04.py

--- Pr04.py
+++ Pr04.py
@@ -1,15 +1,14 @@
-import math
-
+import time
 def maximum(p,m):
     if p > m:
         return p
-    else:
-        return m
-    
-max_pali = int(1)
-for i in range(100,1000):
-    for j in range(100,1000):
+    return m
+
+start = time.time()
+max_pali = 1 ; start_val = 100 ; stop_val = 1000
+for i in range(start_val,stop_val):
+    for j in range(start_val,stop_val):
         if (str(i*j) == str(i*j)[::-1]):
             max_pali = maximum(i*j,max_pali)
-            
-print(max_pali)
+
+print(max_pali,time.time()-start,"seconds")
