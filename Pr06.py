16d7d8903894add986fe1fe267fa18acbf781153
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2018-11-01 22:56:33 +0200

    Update Pr06.py

--- Pr06.py
+++ Pr06.py
@@ -1,6 +1,7 @@
-import math
+import math , time
 
-N = 100
+start = time.time() ; N = 100
 sum_of_squares = sum(x**2 for x in range(1,N+1))
 square_of_sum = sum(x for x in range(1,N+1))
 print(int(math.fabs(square_of_sum**2 - sum_of_squares)))
+print(time.time() - start,"seconds")
