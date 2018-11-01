2cb0a0b7b0b08dd653d268ba7c8f425ba34a9cee
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2018-11-01 22:30:13 +0200

    Update Pr03.py

--- Pr03.py
+++ Pr03.py
@@ -1,12 +1,11 @@
-import math
 import time
-
 start_time = time.time()
-N = 600851475143
-p = 2
+N = 600851475143 ; p = 2
+# Starting by 2 , we will divide each time the p number
+# doing prime factorization
 while (N >= p*p):
     if ( N%p == 0):
-        N = N/p;
+        N = N/p
     else:
         p+=1
 print(N)
