d107b30472bd8fe5962da61fd4164cefe6425ffe
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2018-11-01 23:02:29 +0200

    Update Pr08.py

--- Pr08.py
+++ Pr08.py
@@ -1,14 +1,16 @@
-f = open("Pr08_numbers.txt","r")
+import time
+
+start = time.time()
+f = open("Pr8_numbers.txt","r")
 data = list(f.read())
 while('\n' in data):
     data.remove('\n')
-max_value = []
-N = 13
+max_value = [] ; N = 13
 for i in range(0,len(data)-N):
     value = 1
     lst = data[i:i+N]
     for c in lst:
         value*=int(c)
     max_value.append(value)
-print(max(max_value))
+print(max(max_value),time.time()-start,"seconds")
 
