4f18205c1ee63d54b8ce9d370ad095cd6a325fcd
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2018-11-01 22:26:44 +0200

    Update Pr02.py

--- Pr02.py
+++ Pr02.py
@@ -2,8 +2,9 @@ import math,time
 start_time = time.time()
 
 (f0,f1) = (1,1)
-ans = 0 ; N = 4000000
+ans = 0 ; N = 4*10**6
 while (f1 < N):
+    # Find the sum of even fibonacci terms
     if ( f1%2 == 0):
         ans+=f1
     (f0,f1) = (f1,f1+f0)    
