7232ec325f61c2819036dc65530e1064fab16a11
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2019-04-10 20:18:21 +0300

    Update Pr012.py

--- python/Pr012.py
+++ python/Pr012.py
@@ -1,18 +1,16 @@
-import math
+import time
+N = 500
 
 def sum_of_gcd(num):
-    s = 1 # 1 as a factor
-    for i in range(2,round(math.sqrt(num))):
+    dividers = 2 # 1 and <num> itselfs as a factor
+    for i in range(2,int(num**(0.5))+1):
         if( num%i == 0):
-            s+=2 # d and n/d factors
-    return s+1 # the number itself as a factor
+            dividers+=2 # d and n/d factors
+    return dividers # the number itself as a factor
 
-N = 500
-triag = 1
-i = 1
-while(1):
-    if(sum_of_gcd(triag)> N):
-        print(triag)
-        break
-    i+=1
-    triag+=i
+start = time.time()
+triag = 1 ; i = 1
+while(sum_of_gcd(triag) < N+1):
+	i+=1 ; triag+=i
+print(triag)
+print("Time Evaluated: ", time.time() - start, "seconds") 
