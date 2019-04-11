a6cf347d8892e68e52994eb69342908fb048fdce
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2019-04-12 01:36:27 +0300

    Update Pr018.py

--- python/Pr018.py
+++ python/Pr018.py
@@ -1,10 +1,14 @@
+import time
+
 def triag(data):
 	for i in range(len(data)-2,-1,-1):
 		for j in range(i+1):
 			data[i][j]+=max(data[i+1][j],data[i+1][j+1])
 	return data[0][0]
 
+start = time.time()
 f = open("Pr018_numbers.txt","r")
 data = [[int(x) for x in line.split()] for line in f]
 print(triag(data))
+print("Time Evaluated :",time.time()-start," seconds")
 
