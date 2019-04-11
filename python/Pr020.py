734c68af640f4a92e5b63bc30b7f405e82597527
LPapakostas <42965639+LPapakostas@users.noreply.github.com>
2019-04-12 01:45:37 +0300

    Update Pr020.py

--- python/Pr020.py
+++ python/Pr020.py
@@ -1,8 +1,14 @@
+import time
+N = 100
+
 def factorial(n):
 	if n < 2:
 		return 1
 	else:
 		return n*factorial(n-1)
-	
-f = [int(x) for x in str(factorial(100))]	
-print(sum(f[x] for x in range(0,len(f))))
+
+start = time.time()
+# <f> list contains all of digitis of N!
+f = [int(x) for x in str(factorial(N))]	
+print(sum(f[i] for i in range(len(f))))
+print("Time Evaluated :",time.time()-start," seconds")
