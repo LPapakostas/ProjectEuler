e3764f52823d5332d4f72ca4e0272fc4ca53271f
LPapakostas <lpapakostas95@yahoo.com>
2021-06-12 15:56:02 +0300

    Add maximum and is_prime functions

--- python/helper_functions.py
+++ python/helper_functions.py
@@ -22,3 +22,22 @@ def timer(func):
         print(f"{func.__name__} took {round(end-start,4)} seconds to run")
         return res
     return wrapper
+
+def maximum(p: int,m: int) -> int:
+    """Returns maximum value between two numbers.
+    """
+    if p > m:
+        return p
+    return m
+
+def is_prime(x: int) -> bool:
+    """Returns True if for a prime number.
+    """
+    if (x == 2):
+        return True
+    if (x<2) or (x%2 == 0) :
+        return False
+    for i in range(3,int(x**(0.5))+1,2):
+        if (x%i == 0):
+            return False
+	return True
