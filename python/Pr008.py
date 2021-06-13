aa210e77ff36972bfb60fce09458f3e9aa723474
LPapakostas <lpapakostas95@yahoo.com>
2021-06-13 14:50:17 +0300

    refactor problem 8 code

--- python/Pr008.py
+++ python/Pr008.py
@@ -1,16 +1,61 @@
+from os import read
 import time
+from Pr004 import ANS2
+from helper_functions import timer
+from typing import Iterable
 
-start = time.time()
-f = open("Pr008_numbers.txt","r")
-data = list(f.read())
-while('\n' in data):
-    data.remove('\n')
-max_value = [] ; N = 13
-for i in range(0,len(data)-N):
-    value = 1
-    lst = data[i:i+N]
-    for c in lst:
-        value*=int(c)
-    max_value.append(value)
-print(max(max_value),time.time()-start,"seconds")
+def read_file(path: str) -> Iterable:
+    """Helper function that returns data 
+    from file.
+    """
+    newline = '\n'
+    with open(path,"r") as f:
+        data = list(f.read())
+    while(newline in data):
+        data.remove(newline)
+    
+    return data
+
+@timer
+def find_largest_series_product(N: int, data: Iterable) -> int:
+    """Find the largest product of adjucent one digit
+    numbers.
+    
+    Parameters
+    ----------
+    N : `int`
+        Adjucent numbers length.
+        
+    data : `Iterable`
+        Data that need to be processed. 
+        
+    Returns
+    -------
+    max_value : `int`
+        Largest adjucent number product
+    """
+    # Find all values that obtained from
+    # adjacent numbers.
+    adj_values = []
+    for i in range(0,len(data) - N):
+        temp_value = 1
+        temp_lst = data[i:i+N]
+        for num in temp_lst:
+            temp_value *= int(num)
+        adj_values.append(temp_value)
+        
+    max_value = max(adj_values)
+    return max_value
+    
+N1, ANS1 = 4, 5832
+N2, ANS2 = 13, 23514624000
+
+if (__name__ == "__main__"):
+    fname = "Pr008_numbers.txt"
+    data_pr8 = read_file(fname)
+    
+    assert(find_largest_series_product(N1, data_pr8) == ANS1)
+    ans = find_largest_series_product(N2, data_pr8)
+    print(f"Problem 8 answer is {ans}")
+    assert(ans == ANS2)
 
