abf1ffcfaa2920f834d7985fe6164c9916ca0225
LPapakostas <lpapakostas95@yahoo.com>
2021-06-13 14:50:44 +0300

    refactor problem 7 code

--- python/Pr007.py
+++ python/Pr007.py
@@ -1,24 +1,34 @@
-import time
-N = 10001
+from Pr004 import ANS2
+from helper_functions import timer, is_prime
 
-# This function returns True if x is a prime number
-def is_prime(x):
-	if (x == 2):
-		return True
-	if (x<2) or (x%2 == 0) :
-		return False
-	for i in range(3,int(x**(0.5))+1,2):
-		if (x%i == 0):
-			return False
-	return True
-
-
-start = time.time()
-# We start from 3 in order to increase our step by 2 
-n_of_prime = 2 ; num = 3
-while (n_of_prime < N):
-	num+=2
-	if is_prime(num):
-		n_of_prime +=1
+@timer
+def get_prime(position: int) -> int:
+	"""Returns the <position>th prime number
+    
+    Parameters
+    ----------
+    `position`: int
+		Prime number position, starting from 2.
 	
-print(num) ; print("Time Evaluated :", time.time() - start, "seconds")
+	Returns
+	-------
+	Prime number of specific position
+    """
+    # We start from 3 in order to increase our step by 2 
+	n_of_primes = 2
+	prime_value = 3
+ 
+	while( n_of_primes < position):
+		prime_value += 2
+		if is_prime(prime_value):
+			n_of_primes +=1
+	return prime_value
+
+N1, ANS1 = 6, 13
+N2, ANS2 = 10001, 104743
+
+if (__name__ == "__main__"):
+    assert(get_prime(N1) == ANS1)
+    ans = get_prime(N2)
+    print(f"Problem 7 answer is {ans}")
+    assert(ans == ANS2)
