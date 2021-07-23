a17d15822a014d2143bcd5cea137a063583b8880
LPapakostas <lpapakostas95@yahoo.com>
2021-07-23 23:26:00 +0300

    Refactor Problem 17

--- python_solutions/Pr017.py
+++ python_solutions/Pr017.py
@@ -1,36 +1,73 @@
-import time
-one_to_nin = ["one","two","three","four","five","six","seven","eight","nine","ten",
-              "eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen",
-            "eighteen","nineteen"]
-tens = ["twenty","thirty","forty","fifty","sixty","seventy","eighty","ninety"]
-a = "and" ; h = "hundred"
+from helper_functions import timer
 
-def letters_of_num(i):
-    ans = 0
-    if (len(str(i)) == 2):
-        un = int(i%10) ; dec = int(i/10)
-        if (un == 0):
-            ans = len(tens[dec-2])
-        else:
-            ans = (len(tens[dec-2]) + len(one_to_nin[un-1]))
+
+def letters_of_number(num: int) -> int:
+    """
+    Compute the total number of letters for a specific number.
+
+    Note
+    ----
+    For example, 101 is referenced as "one hundred and one" due
+    to british convention
+    """
+    # In case num < 10, return corresponding word length
+    if (num < 20):
+        return len(ONE_TO_NINETEEN[num])
+    # In case num < 100, split number into 2 digits
+    elif (num >= 20 and num < 100):
+        dec = int(num/10)
+        res = int(num % 10)
+        ans = len(TENS[dec]) + len(ONE_TO_NINETEEN[res])
+        return ans
+    # In case num < 1000, split number accordingly
+    elif (num < 1000):
+        hun = int(num/100)
+        res = int(num % 100)
+        ans = len(ONE_TO_NINETEEN[hun]) + len(HUNDRED)
+        if (res > 0 and res < 20):
+            ans += len(AND) + len(ONE_TO_NINETEEN[res])
+        elif (res >= 20):
+            dec = int(res/10)
+            un = int(res % 10)
+            ans += len(AND) + len(TENS[dec]) + len(ONE_TO_NINETEEN[un])
+        return ans
+    elif (num == 1000):
+        return len(THOUSAND)
     else:
-        hun = int(i/100) ; res = int(i%100)
-        if (res == 0):
-            ans = (len(one_to_nin[hun-1]) + len(h))
-        elif (res>=1) and (res<=19) :
-            ans = (len(one_to_nin[hun-1]) + len(h) + len(a) + len(one_to_nin[res-1]))
-        else:
-            dec = int(res/10) ; un = int(res%10)
-            if(un != 0):
-                ans = (len(one_to_nin[hun-1]) + len(h) + len(a) + len(tens[dec-2]) + len(one_to_nin[un-1]))
-            else:
-                ans = (len(one_to_nin[hun-1]) + len(h) + len(a) + len(tens[dec-2]))
-    return ans
-  
-start = time.time() ; N = 1000 ; ans = 0 
-ans += sum([len(x) for x in one_to_nin])
-for i in range(20,N):
-    ans += letters_of_num(i)
-print(ans+len('onethousand'),time.time()-start,"seconds")
-        
-    
+        print("Not supported in this version")
+
+    return 0
+
+
+@timer
+def count_number_letters(N: int) -> int:
+    """
+    Return the total letters for numbers starting from 
+    1 to <N>
+    """
+    count = 0
+    for number in range(1, N+1):
+        count += letters_of_number(number)
+    return count
+
+
+if (__name__ == "__main__"):
+
+    # Define global lists that contain alphanumeric values
+    # of standard numbers
+    ONE_TO_NINETEEN = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+                       "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                       "eighteen", "nineteen"]
+    TENS = ["", "", "twenty", "thirty", "forty", "fifty",
+            "sixty", "seventy", "eighty", "ninety"]
+    AND = "and"
+    HUNDRED = "hundred"
+    THOUSAND = "onethousand"
+
+    N1, ANS1 = 5, 19
+    N2, ANS2 = 1000, 21124
+    assert(ANS1 == count_number_letters(N1))
+    ans = count_number_letters(N2)
+    assert(ANS2 == ans)
+
+    print(f"Problem 17 answer is {ans}")
