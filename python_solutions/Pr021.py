2556a1e11c2775584898b48f87d61d4ea9804e71
LPapakostas <lpapakostas95@yahoo.com>
2021-08-08 01:57:14 +0300

    Refactor Problem 21

--- python_solutions/Pr021.py
+++ python_solutions/Pr021.py
@@ -1,23 +1,53 @@
-import time
-N = 10000
-
-def sum_of_gcd(x):
-  s=1 # every number has 1 as a divisor
-  # find divisors till sqrt of <x> because 
-  # if one divisor is i, the other is x/i
-  for i in range(2,int(x**0.5)+1):
-    if ((x%i) == 0):
-      s+=int(x/i) + i
-  return s
+from helper_functions import timer
 
 
-start = time.time() ; amiable = []
-# for amiable numbers, the number itselfs is not 
-# included
-for a in range(2,N+1):
-	b = sum_of_gcd(a)
-	if( a==sum_of_gcd(b) and (a!=b) and (a not in amiable)):
-		amiable.append(a)
-		amiable.append(b)
-print(sum(amiable))
-print("Time Evaluated :",time.time()-start," seconds")
+@timer
+def amicable_number_sum(N: int) -> int:
+    """
+    Find the sum of amiciable numbers given a specific upper limit.
+
+    Notes
+    -----
+    Let d(n) be defined as the sum of proper divisors of <n> 
+    (numbers less than <n> which divide evenly into <n>).
+
+    If d(a) = b and d(b) = a, where a ≠ b, a and b are called amicable numbers.
+    """
+    amicable_numbers = set()
+
+    for num_a in range(N+1):
+        num_b = sum_of_gcd(num_a)
+        is_amicable = (num_a == sum_of_gcd(num_b) and num_b != num_a)
+        if is_amicable:
+            amicable_numbers.add(num_a)
+            amicable_numbers.add(num_b)
+
+    amiciable_sum = sum(list(amicable_numbers))
+    return amiciable_sum
+
+
+def sum_of_gcd(x: int) -> int:
+    """
+    Helper function that, given a number, it computes
+    the summary of each greatest common divisors.
+    """
+    # Initialize sum with <1>, since every nunber
+    # has one as divisor
+    gcd_sum = 1  # 1
+    limit = int(x**0.5) + 1
+
+    # Find divisors until sqrt(x) because
+    # if one divisor is i, the other is x/i
+    for i in range(2, limit):
+        if (x % i == 0):
+            gcd_sum += int(x/i) + i
+
+    return gcd_sum
+
+
+if (__name__ == "__main__"):
+    N, ANS = 10000, 31626
+
+    ans = amicable_number_sum(N)
+    print(f"Problem 21 answer is {ans}")
+    assert(ans == ANS)
