f1806dee2a79789253674cb20210287051d8d21e
LPapakostas <lpapakostas95@yahoo.com>
2021-08-08 03:02:53 +0300

    Refactor Problem 25

--- python_solutions/Pr025.py
+++ python_solutions/Pr025.py
@@ -1,7 +1,34 @@
-digits = 1000
-f1,f2,index = 1,1,2
-while(True):
-	f1,f2,index = f2,f1+f2,index+1
-	if(len(str(f2)) == digits):
-		print(index)
-		break
+from helper_functions import timer
+
+
+@timer
+def fibonacci_index(digit_num: int) -> int:
+    """Returns the index of Fibonacci number that 
+    has the same number of digits as input
+
+    Parameters
+    ----------
+    digit_num : `int`
+        Given number of digits
+    """
+    # Initialize fibonacci terms
+    fn1, fn2 = 1, 1
+    index = 2
+    condition = not (len(str(fn2)) == digit_num)
+
+    while(condition):
+        fn1, fn2 = fn2, fn1+fn2
+        index += 1
+        condition = not (len(str(fn2)) == digit_num)
+
+    return index
+
+
+if (__name__ == "__main__"):
+    N1, ANS1 = 3, 12
+    N2, ANS2 = 1000, 4782
+
+    assert(ANS1 == fibonacci_index(N1))
+    ans = fibonacci_index(N2)
+    print(f"Problem 25 answer is {ans}")
+    assert(ans == ANS2)
